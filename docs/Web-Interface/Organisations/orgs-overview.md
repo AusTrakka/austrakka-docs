dd5cac4aaa37b76467a0660f4bbed5bd3c45d70d
Clare Sloggett <claresloggett@gmail.com>
2025-07-15 11:39:44 +1000

    Add text and structure based on Connie's inputs

---
+++ docs/Web-Interface/Organisations/orgs-overview.md
@@ -0,0 +1,6 @@
+
+# Overview
+
+Organisations within AusTrakka represent institutions or groups of users, which may wish to manage their own user's role, and manage their own data within the system.
+
+Public health laboratories are commonly registered as Trakka organisations.
--- sidebars.ts
+++ sidebars.ts
@@ -19,70 +19,91 @@ const sidebars: SidebarsConfig = {
     },
     {
       type: 'category',
-      label: 'Project views',
+      label: 'Web interface',
       items: [
         {
           type: 'doc',
-          label: 'Project dashboards',
-          id: 'Projects/project-dashboard',
+          label: 'Introduction',
+          id: 'Web-Interface/web-introduction',
         },
         {
-          type: 'doc',
-          label: 'Samples table',
-          id: 'Projects/samples-table',
+          type: 'category',
+          label: 'Project views',
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'Web-Interface/Projects/projects-overview',
+            },
+            {
+              type: 'doc',
+              label: 'Project dashboards',
+              id: 'Web-Interface/Projects/project-dashboard',
+            },
+            {
+              type: 'doc',
+              label: 'Samples table',
+              id: 'Web-Interface/Projects/samples-table',
+            },
+            {
+              type: 'doc',
+              label: 'Tree viewer',
+              id: 'Web-Interface/Projects/tree-viewer',
+            },
+            {
+              type: 'doc',
+              label: 'Plots',
+              id: 'Web-Interface/Projects/plots',
+            },
+            {
+              type: 'doc',
+              label: 'Members',
+              id: 'Web-Interface/Projects/members',
+            },
+          ],
         },
         {
-          type: 'doc',
-          label: 'Tree viewer',
-          id: 'Projects/tree-viewer',
+          type: 'category',
+          label: 'Organisation views',
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'Web-Interface/Organisations/orgs-overview',
+            },
+            {
+              type: 'doc',
+              label: 'Members',
+              id: 'Web-Interface/Organisations/members',
+            },
+            {
+              type: 'doc',
+              label: 'Data view',
+              id: 'Web-Interface/Organisations/data-views',
+            }
+          ],
         },
         {
-          type: 'doc',
-          label: 'Plots',
-          id: 'Projects/plots',
-        },
-        {
-          type: 'doc',
-          label: 'Members',
-          id: 'Projects/members',
+          type: 'category',
+          label: 'Upload pages',
+          items: [
+            {
+              type: 'doc',
+              label: 'Metadata uploads',
+              id: 'Web-Interface/Uploads/metadata-uploads',
+            },
+            {
+              type: 'doc',
+              label: 'Sequence uploads',
+              id: 'Web-Interface/Uploads/sequence-uploads',
+            }
+          ],
         },
       ],
     },
     {
       type: 'category',
-      label: 'Organisation views',
-      items: [
-        {
-          type: 'doc',
-          label: 'Members',
-          id: 'Organisations/members',
-        },
-        {
-          type: 'doc',
-          label: 'Data view',
-          id: 'Organisations/data-views',
-        }
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Uploading data',
-      items: [
-        {
-          type: 'doc',
-          label: 'Metadata uploads',
-          id: 'Uploads/metadata-uploads',
-        },
-        {
-          type: 'doc',
-          label: 'Sequence uploads',
-          id: 'Uploads/sequence-uploads',
-        }
-      ],
-    },
-    {
-      type: 'category',
-      label: 'AusTrakka CLI',
+      label: 'Command-line interface',
       items: [
         {
           type: 'doc',
@@ -108,6 +129,22 @@ const sidebars: SidebarsConfig = {
           type: 'doc',
           label: 'Managing sample sharing',
           id: 'CLI/CLI-sharing-records',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Reference',
+      items: [
+        {
+          type: 'doc',
+          label: 'Sequence data',
+          id: 'Reference/sequence-data'
+        },
+        {
+          type: 'doc',
+          label: 'Roles and permissions',
+          id: 'Reference/roles-and-permissions'
         },
       ],
     },
