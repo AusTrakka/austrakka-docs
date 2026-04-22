e7fe1bb4fca34174a50f87eb0a05575ee7e40ff3
Samuel Carswell <samuelrcarswell@gmail.com>
2026-04-22 13:15:15 +1000

    AB#8099 linting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
-
 export default function Home(): JSX.Element {
   return (
     <Redirect to="/docs/Web-Interface/web-introduction" />
